cf473a424c35d7ffb22aac52ee50e55119a86e94
Diego Campo <diegocampo@Diegos-MacBook-Pro.local>
2019-07-01 00:28:38 -0500

    Chapter 9: Inheritance added

---
+++ src/chapter9/InheritanceTester.java
@@ -0,0 +1,33 @@
+package chapter9;
+
+public class InheritanceTester {
+
+    public static void main(String args[]){
+
+        Employee employee = new Employee();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print("square");
+
+        Mother mom = new Mother();
+        mom.setName("Yineth");
+
+        System.out.println(mom.getName() + " is a " + mom.getGender() );
+
+    }
+
+    public static void testSquareOverride(){
+        Rectangle rectangle = new Rectangle();
+        rectangle.setLength(4);
+        rectangle.setWidth(9);
+        System.out.println(rectangle.calculatePerimeter());
+
+        Square square = new Square();
+        square.setLength(4);
+        square.setWidth(8);
+        System.out.println(square.calculatePerimeter());
+    }
+}
